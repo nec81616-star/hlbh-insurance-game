d6cb3f4ee6e56031338399d3969a62fbeba33880
nec81616-star <nec81616@gmail.com>
2026-09-22 16:42:51 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.GAME_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCcQjA6GoCv48zj6zuQafSDJpzS7qUdnho",
+  authDomain: "hlbh-insurance-game.firebaseapp.com",
+  databaseURL: "https://hlbh-insurance-game-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "hlbh-insurance-game",
+  storageBucket: "hlbh-insurance-game.firebasestorage.app",
+  messagingSenderId: "534981233585",
+  appId: "1:534981233585:web:3b2233955a1aa8aa044b90"
+};
